8545004fce684daebaff477501befc6ea96e0d9e
Julius <julius.foverskov1@gmail.com>
2025-10-09 17:58:44 +0200

    add dataclasses to config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from dataclasses import dataclass
 
 # Project directories
 BASE_DIR = Path(__file__).resolve().parent
@@ -11,9 +12,12 @@ DOWNLOADS_DIR.mkdir(exist_ok=True)
 
 # Input data file
 DATA_FILE = DATA_DIR / "GRI_2017_2020.xlsx"
+
+# Output log file
 LOG_FILE = LOGS_DIR / "log.json"
 
 # Dataframe columns
+SHEET_NAME = 0  
 ID_COLUMN = "BRnum"
 PDF_URL_COLUMN = "Pdf_URL"
 SECONDARY_PDF_URL_COLUMN = "Report Html Address"
@@ -23,3 +27,21 @@ DOWNLOAD_TIMEOUT = 0.5  # seconds
 REQUEST_HEADERS = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36" # To mimic a real browser and not a web scraper
 } 
+
+@dataclass(frozen=True)
+class DataConfig:
+    data_file: Path
+    log_file: Path 
+    sheet_name: int 
+    id_column: str 
+    pdf_url_column: str 
+    secondary_pdf_url_column: str
+
+@dataclass(frozen=True)
+class DownloadConfig:
+    downloads_dir: Path
+    download_timeout: float
+    request_headers: dict
+
+
+
